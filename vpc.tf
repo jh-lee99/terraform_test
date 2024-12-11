f2360c0895ed9db546d6aba3d7fa2eb4e160fa3a
jh-lee99 <jun99h1219@gmail.com>
2024-12-11 23:29:30 +0900

    edit vpc tf file

--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,36 @@ resource "aws_vpc" "SE_DBA-TEST" {
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.SE_DBA-TEST.id
+  cidr_block        = public_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = 2
+  vpc_id            = aws_vpc.SE_DBA-TEST.id
+  cidr_block        = private_subnet_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "SE_private_subnets_a" {
+  for_each = toset(local.SE_private_subnet_cidr_a)
+
+  vpc_id            = aws_vpc.SE_DBA-TEST.id
+  cidr_block        = each.key
+  availability_zone = local.azs[0]
+  map_public_ip_on_launch = false
+  
+  tags = {
+    Name  = "${local.SE_owners[index(local.SE_private_subnet_cidr_a, each.key)]}-TEST-PRI-${local.az_suffixes[0]}}"
+    Owner = local.SE_owners[index(local.SE_private_subnet_cidr_a, each.key)]
+  }
+}
+
 resource "aws_subnet" "SE_private_subnets_a" {
   for_each = toset(local.SE_private_subnet_cidr_a)
 
