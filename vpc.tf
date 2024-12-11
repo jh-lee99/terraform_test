ab07a358ce4d5f88e2d8e58a4dc95d68c866d611
jh-lee99 <jun99h1219@gmail.com>
2024-12-11 23:30:25 +0900

    edit vpc tf file

--- vpc.tf
+++ vpc.tf
@@ -36,20 +36,6 @@ resource "aws_subnet" "SE_private_subnets_a" {
   }
 }
 
-resource "aws_subnet" "SE_private_subnets_a" {
-  for_each = toset(local.SE_private_subnet_cidr_a)
-
-  vpc_id            = aws_vpc.SE_DBA-TEST.id
-  cidr_block        = each.key
-  availability_zone = local.azs[0]
-  map_public_ip_on_launch = false
-  
-  tags = {
-    Name  = "${local.SE_owners[index(local.SE_private_subnet_cidr_a, each.key)]}-TEST-PRI-${local.az_suffixes[0]}}"
-    Owner = local.SE_owners[index(local.SE_private_subnet_cidr_a, each.key)]
-  }
-}
-
 resource "aws_subnet" "SE_private_subnets_c" {
   for_each = toset(local.SE_private_subnet_cidr_c)
 
