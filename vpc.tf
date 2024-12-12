2f0910e73dcb239cc0f5145e71303983b4436173
jh-lee99 <jun99h1219@gmail.com>
2024-12-12 09:05:32 +0900

    edit vpc tf file

--- vpc.tf
+++ vpc.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "SE_private_subnets_a" {
   count = length(local.SE_private_subnet_cidr_a)
 
   vpc_id            = aws_vpc.SE_DBA-TEST.id
-  cidr_block        = each.key
+  cidr_block        = local.SE_private_subnet_cidr_a[count.index]
   availability_zone = local.azs[0]
   map_public_ip_on_launch = false
   
@@ -47,46 +47,46 @@ resource "aws_subnet" "SE_private_subnets_a" {
 }
 
 resource "aws_subnet" "SE_private_subnets_c" {
-  for_each = toset(local.SE_private_subnet_cidr_c)
+  count = length(local.SE_private_subnet_cidr_c)
 
   vpc_id            = aws_vpc.SE_DBA-TEST.id
-  cidr_block        = each.key
+  cidr_block        = local.SE_private_subnet_cidr_c[count.index]
   availability_zone = local.azs[1]
   map_public_ip_on_launch = false
   
   tags = {
-    Name  = "${local.SE_owners[index(local.SE_private_subnet_cidr_c, each.key)]}-TEST-PRI-${local.az_suffixes[1]}"
-    Owner = local.SE_owners[index(local.SE_private_subnet_cidr_c, each.key)]
+    Name  = "${local.SE_owners[count.index]}-TEST-PRI-${local.az_suffixes[1]}"
+    Owner = local.SE_owners[count.index]
     group = "SE"
   }
 }
 
 resource "aws_subnet" "DBA_private_subnets_a" {
-  for_each = toset(local.DBA_private_subnet_cidr_a)
+  count = length(local.DBA_private_subnet_cidr_a)
 
   vpc_id            = aws_vpc.SE_DBA-TEST.id
-  cidr_block        = each.key
+  cidr_block        = local.DBA_private_subnet_cidr_a[count.index]
   availability_zone = local.azs[0]
   map_public_ip_on_launch = false
   
   tags = {
-    Name  = "${local.DBA_owners[index(local.DBA_private_subnet_cidr_a, each.key)]}-TEST-PRI-${local.az_suffixes[0]}"
-    Owner = local.DBA_owners[index(local.DBA_private_subnet_cidr_a, each.key)]
+    Name  = "${local.DBA_owners[count.index]}-TEST-PRI-${local.az_suffixes[0]}"
+    Owner = local.DBA_owners[count.index]
     group = "DBA"
   }
 }
 
 resource "aws_subnet" "DBA_private_subnets_c" {
-  for_each = toset(local.DBA_private_subnet_cidr_c)
+  count = length(local.DBA_private_subnet_cidr_c)
 
   vpc_id            = aws_vpc.SE_DBA-TEST.id
-  cidr_block        = each.key
+  cidr_block        = local.DBA_private_subnet_cidr_c[count.index]
   availability_zone = local.azs[1]
   map_public_ip_on_launch = false
   
   tags = {
-    Name  = "${local.DBA_owners[index(local.DBA_private_subnet_cidr_c, each.key)]}-TEST-PRI-${local.az_suffixes[1]}"
-    Owner = local.DBA_owners[index(local.DBA_private_subnet_cidr_c, each.key)]
+    Name  = "${local.DBA_owners[count.index]}-TEST-PRI-${local.az_suffixes[1]}"
+    Owner = local.DBA_owners[count.index]
     group = "DBA"
   }
 }
